5cd89323badd736f5800cd6680a86e568462c7fc
evilchrisdesuyo <70076456+evilchrisdesuyo@users.noreply.github.com>
2020-08-23 02:35:23 -0700

    NPC and gamemode updates
    Created Player prefab
    Created NPC prefab
    Created NPC spawner
    Added flying saucer for point drop off (no script yet)

--- Assets/Scripts/NPCSpawner.cs
+++ Assets/Scripts/NPCSpawner.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class NPCSpawner : MonoBehaviour
 {
+    public GameObject Player1;
+    public List<GameObject> spawnList;
+    public float distanceToPlayer;
+    public float spawnFrequency = 5f;
+    public float currentSpawnTimer;
+    public float spawnRange = 75;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSpawnTimer = spawnFrequency;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //check distance to player
+        currentSpawnTimer -= 1 * Time.deltaTime;
+        if (currentSpawnTimer < 0f)
+        {
+            currentSpawnTimer = spawnFrequency;
+        }
+            //check distance to player
+            distanceToPlayer = Vector3.Distance(Player1.transform.position, transform.position);
         // if player distance is > startSpawningDistance
-            //start spawning()
+        if (distanceToPlayer > spawnRange && currentSpawnTimer <= 0.2f)
+        {
+            Debug.Log("Spawning NPC");
+            spawnNPC();
+        }
+        //start spawning()
+    }
+
+   void spawnNPC()
+    {
+        
+        Instantiate(spawnList[Random.Range(0, spawnList.Count)], this.transform.position, Quaternion.identity);
+        currentSpawnTimer = spawnFrequency;
     }
     //start spawning
     //spawn NPC
